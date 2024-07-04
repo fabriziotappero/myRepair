52a466815bcbada23507a249e4f71cbf717e728a
Fabrizio Tappero <Fabrizio.Tappero@itk-engineering.com>
2024-07-04 10:59:14 +0200

    move css js and images in assets

--- assets/sw.js
+++ assets/sw.js
@@ -2,9 +2,9 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('v1').then((cache) => {
             return cache.addAll([
-                '/',
-                'index.html',
-                'custom.css',
+                '/myRepair/',
+                '/myRepair/index.html',
+                '/myRepair/assets/custom.css',
                 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css',
                 'https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css',
                 'https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js'
