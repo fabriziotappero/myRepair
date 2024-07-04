f9a7aefca44585796ebb14d1416cd88c35f86c7f
Fabrizio Tappero <Fabrizio.Tappero@itk-engineering.com>
2024-07-04 08:43:18 +0200

    modify PWA stuff

--- sw.js
+++ sw.js
@@ -1,19 +1,21 @@
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open('pwa-example').then(function (cache) {
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
             return cache.addAll([
                 '/',
                 '/index.html',
-                '/app.js',
-                '/app.css' //Add any other assets your web page needs
+                '/custom.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css',
+                'https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css',
+                'https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js'
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
+        caches.match(event.request).then((response) => {
             return response || fetch(event.request);
         })
     );
